ecad8cf0c0754009c43ed7d8707bf2afa0d5bfa9
Ilya <efgvj45@mail.ru>
2022-05-20 17:39:23 +0300

    Refactor class Util, UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -4,22 +4,12 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.*;
-import java.util.LinkedList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() throws SQLException, ClassNotFoundException {
-
     }
-    private static Connection connection;
-
-    static {
-        try {
-            connection = Util.getConnection();
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private final Connection connection = Util.getConnection();;
 
     public void createUsersTable() throws SQLException, ClassNotFoundException {
             Statement statement = connection.createStatement();
@@ -32,20 +22,25 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-            Statement statement = connection.createStatement();
-            String sql = String.format("INSERT INTO ex_jdbc.user (`name`, `lastName`, `age`) VALUES (\"%s\", \"%s\", %d);", name, lastName, age);
-            statement.executeUpdate(sql);
+        String sql = "INSERT INTO ex_jdbc.user (`name`, `lastName`, `age`) VALUES (?, ?, ?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setByte(3, age);
+        preparedStatement.executeUpdate();
     }
 
     public void removeUserById(long id) throws SQLException {
-        Statement statement = connection.createStatement();
-        String sql = String.format("DELETE FROM user WHERE id = %s", id);
-        statement.executeUpdate(sql);
+        String sql = "DELETE FROM user WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setLong(1, id);
+        preparedStatement.executeUpdate();
     }
 
     public List<User> getAllUsers() throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM user");
+        String sql = "SELECT * FROM user";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
             List<User> list = new java.util.ArrayList<>();
         while (resultSet.next()) {
             User user = new User();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,28 +6,17 @@ import java.sql.SQLException;
 
 public class Util {
 
+    private static final String userName = "****";
+    private static final String password = "****";
+    private static final String connectionURL = "jdbc:mysql://localhost:3306/ex_jdbc";
+
     public static Connection getConnection() throws SQLException,
             ClassNotFoundException {
-        String hostName = "localhost";
-
-        String dbName = "ex_jdbc";
-        String userName = "****";
-        String password = "****";
-
-        return getConnection(hostName, dbName, userName, password);
-    }
-
-    public static Connection getConnection(String hostName, String dbName,
-                                                String userName, String password) throws SQLException, ClassNotFoundException {
 
         Class.forName("com.mysql.jdbc.Driver");
 
-        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
-
-        Connection connection = DriverManager.getConnection(connectionURL, userName,
+        return DriverManager.getConnection(connectionURL, userName,
                 password);
-        connection.setAutoCommit(true);
-
-        return connection;
     }
+
 }
